0af1155e54c545dffedd8b1070351f4ffc0ee2e4
yogeekenta <cune1113@gmail.com>
2019-06-11 20:03:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
