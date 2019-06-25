11b798f5d11bb1f7dfc72ad190eb3638ba7e648e
yogeekenta <cune1113@gmail.com>
2019-06-25 09:56:24 +0900

    add-incremental

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -14,7 +14,7 @@ $(function() {
    }
 
   function appendMembers(name, user_id) {
-    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user_id }'>
                 <input name='group[user_ids][]' type='hidden' value=${ user_id }>
                 <p class='chat-group-user__name'>${ name }</p>
                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
@@ -44,9 +44,12 @@ $(function() {
 
       .done(function(users) {
         $("#user-search-result").empty();
-          if (input !== preinput && users.length !== 0 && input.length !== 0) {
+          if (input !== preinput && users.length !== 0 && input.length !== 0 ) {
             users.forEach(function(user){
-            appendUsers(user);
+              var id = user.id
+              if(!($('#chat-group-user-' + id ).length)){
+                appendUsers(user);
+              }
             });
           }
           else {
